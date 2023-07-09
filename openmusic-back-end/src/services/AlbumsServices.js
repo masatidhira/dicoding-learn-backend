7ac98b40561b85710b650768c87884d3d2c08990
masatidhira <masatidhira@gmail.com>
2023-07-09 19:26:15 +0700

    Add services

---
+++ openmusic-back-end/src/services/AlbumsServices.js
@@ -0,0 +1,76 @@
+/* eslint-disable indent */
+/* eslint-disable comma-dangle */
+/* eslint-disable object-curly-spacing */
+/* eslint-disable max-len */
+/* eslint-disable require-jsdoc */
+const { Pool } = require('pg');
+const { nanoid } = require('nanoid');
+const { mapAlbumsDBToModel } = require('../utils');
+const InvariantError = require('../exceptions');
+const NotFoundError = require('../exceptions');
+
+class AlbumsServices {
+  constructor() {
+    this._pool = new Pool();
+  }
+
+  async addAlbum({ name, year }) {
+    const id = nanoid(16);
+    const insertedAt = new Date().toISOString();
+    const updatedAt = insertedAt;
+
+    const query = {
+      text: 'INSERT INTO albums VALUES($1, $2, $3, $4, $5) RETURNING id',
+      values: [id, name, year, insertedAt, updatedAt],
+    };
+    console.log(query.values);
+
+    const result = this._pool.query(query);
+    if (!result.rows[0].id) {
+      throw new InvariantError('Failed to add album');
+    }
+
+    return result.rows[0].id;
+  }
+
+  async getAlbumById(id) {
+    const query = {
+      text: 'SELECT * FROM albums WHERE id = $1',
+      values: [id],
+    };
+
+    const result = await this._pool.query(query);
+    if (!result.rows.length) {
+      throw new NotFoundError('Album not found');
+    }
+
+    return result.rows.map(mapAlbumsDBToModel)[0];
+  }
+
+  async editAlbumById(id, { name, year }) {
+    const updatedAt = new Date().toISOString();
+    const query = {
+      text: 'UPDATE albums SET name = $1, year = $2, updated_at = $3 WHERE id = $4 RETURNING id',
+      values: [name, year, updatedAt, id],
+    };
+
+    const result = await this._pool.query(query);
+    if (!result.rows.length) {
+      throw new NotFoundError('Failed to update album. album id not found');
+    }
+  }
+
+  async deleteAlbumById(id) {
+    const query = {
+      text: 'DELETE FROM albums WHERE id = $1 RETURNING id',
+      values: [id],
+    };
+
+    const result = await this._pool.query(query);
+    if (!result.rows.length) {
+      throw new NotFoundError('Failed to delete album. Album id not found');
+    }
+  }
+}
+
+module.exports = AlbumsServices;
